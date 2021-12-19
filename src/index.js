7862b19fa57f9a1a75511783bb16bcb7b14adf45
Fred <higgins.fred.s@gmail.com>
2021-12-19 15:28:23 -0800

    Completed Assignment with some extra

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
 
 //Extra Deliverables
 //A delete function that will remove tasks from your list
-//A priority value selected from a dropdown (Links to an external site.) that is used to determine the color of the text in the list (e.g. red for high priority, yellow for medium, green for low)
+//A priority value selected from a dropdown that is used to determine the color of the text in the list (e.g. red for high priority, yellow for medium, green for low)
 //As an additional challenge, implement a sorting functionality that displays the tasks in ascending or descending order based on priority
 //An additional input field (e.g. user, duration, date due)
 //Ability to edit tasks
@@ -13,22 +13,26 @@
 //Grabs 'form' from DOM
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("form").addEventListener('submit', (e) => {
+    let color = e.target.priority.value;
+    console.log(color)
     e.preventDefault();
-    handleToDo(e.target["new-task-description"].value)
+    handleToDo(e.target["new-task-description"].value, color)
   })
 });
 
 //Appends new task to task list
-function handleToDo(todo){
-  let p = document.createElement("p")
-  let button = document.createElement('button')
-  let drop = document.createElement('select')
+function handleToDo(todo, color){
+  let p = document.createElement("p") //adds new item to todo list
+  let button = document.createElement('button') //adds remove button next to created items in todo list
+  
+  //remove button
   button.textContent = "x"
   button.addEventListener("click", handleDelete)
 
-  p.textContent = `${todo} `
+  //todo item
+  p.style.color = color
+  p.textContent = ` ${todo} `
   p.appendChild(button)
-  p.prependChild()
   document.querySelector("#list").appendChild(p)
 }
 
@@ -37,3 +41,6 @@ function handleDelete(e){
   e.target.parentNode.remove()
 }
 
+function colorTask (input, color) {
+
+}
